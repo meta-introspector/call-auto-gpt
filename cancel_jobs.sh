8397f74f1ac2c7ad9f867286825a6224fdbf5e51
mike dupont <mike.dupont@introspector.local>
2023-10-08 16:12:13 -0400

    adding the check

--- cancel_jobs.sh
+++ cancel_jobs.sh
@@ -1,1 +1,1 @@
-for id in $(gh run list --limit 500 --jq ".[] | select (.status == \"queued\" ) | .databaseId" --json databaseId,status); do echo gh run cancel $id; done
+for id in $(gh run list --limit 5000 --jq ".[] | select (.status == \"queued\" ) | .databaseId" --json databaseId,status); do echo gh run cancel $id; done
--- test/run.sh
+++ test/run.sh
@@ -1,1 +1,8 @@
-sudo act run  -j printJob -P ubuntu-latest=h4ckermike/my_act_github -e ./test/fixtures/first.json 
+#sudo act run  -j printJob -P ubuntu-latest=h4ckermike/my_act_github -e ./test/fixtures/first.json 
+sudo act run \
+     -sGITHUB_TOKEN=${GH_TOKEN} \
+     -sGITHUB_PAT=${GH_TOKEN} \
+     -sGITHUB_REPO="meta-introspector/call-auto-gpt" \
+     -j run_bot \
+     -P ubuntu-latest=h4ckermike/my_act_github  \
+     --input-file ./test/example_inputs.env
